e22f10730cef9678c668975bd78f650dd47ec8da
bouctou04 <bmaiga04@gmail.com>
2019-12-24 16:35:17 +0000

    Inclusion des fichiers scripts et styles dans index

--- template/index.php
+++ template/index.php
@@ -4,3 +4,30 @@ define('ROOT', dirname(WEBROOT));
 define('DS', DIRECTORY_SEPARATOR);
 define('CORE', ROOT.DS.'core');
 define('BASE_URL', dirname(dirname($_SERVER['SCRIPT_NAME'])));
+define('BASE_HREF', '<base href="/aptech-app/template/">');
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<?= BASE_HREF; ?>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<link rel="stylesheet" type="text/css" href="font/css/all.css">
+		<title>Aptech-app</title>
+	</head>
+	<body>
+		<!-- Website content -->
+
+
+
+		<!-- End website content -->
+
+
+		<!-- include JavaScript files -->
+		<script src="js/jquery.js" type="text/javascript"></script>
+		<script src="js/main.js" type="text/javascript"></script>
+		<script src="js/app.js" type="text/javascript"></script>
+		<!-- End JavaScript files -->
+	</body>
+</html>
