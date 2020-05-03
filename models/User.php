a1569c441a62886c76de112226822939d8be9102
bouctou04 <bmaiga04@gmail.com>
2020-05-03 15:36:50 +0000

    Finalisation du module inscription

--- models/User.php
+++ models/User.php
@@ -57,6 +57,22 @@ class User extends Model
      */
     private string $password;
 
+
+    /**
+     * Default password charactere generate auto
+     */
+    private const PASSWORD_LENGTH = 8;
+
+    /**
+     * Default min charactere able to repeat on generate password
+     */
+    private const PASSWORD_CHARACTER_REPEAT_MIN = self::PASSWORD_LENGTH - self::PASSWORD_LENGTH;
+
+    /**
+     * Default max charactere able to repeat on generate password
+     */
+    private const PASSWORD_CHARACTER_REPEAT_MAX = self::PASSWORD_LENGTH - 2;
+
     /**
      * User constructor.
      */
@@ -67,6 +83,41 @@ class User extends Model
     }
 
     /**
+     * @param int $user_category_id
+     * @param int $school_id
+     * @param string $last_name
+     * @param string $first_name
+     * @param string $birth_date
+     * @param string $sexe
+     * @param string $email
+     * @throws \Exception
+     */
+    public function insert(int $user_category_id, int $school_id, string $last_name, string $first_name, string $birth_date, string $sexe, string $email) {
+        $this->category_id = $user_category_id;
+        $this->school_id = $school_id;
+        $this->last_name = $last_name;
+        $this->first_name = $first_name;
+        $this->birth_date = $birth_date;
+        $this->sexe = $sexe;
+        $this->email = $email;
+
+        $user_category_id = intval($user_category_id);
+        $school_id = intval($school_id);
+        $last_name = htmlspecialchars($last_name);
+        $first_name = htmlspecialchars($first_name);
+        $birth_date = htmlspecialchars($birth_date);
+        $sexe = htmlspecialchars($sexe);
+        $email = htmlspecialchars($email);
+        $username = $this->getDefaultUsername($this->first_name, $this->last_name, $this->email);
+        $passwordn = self::generatePassword();
+        $password = sha1($passwordn);
+
+        $req = $this->pdo->prepare("INSERT INTO {$this->table}(user_category_id, school_id, last_name, first_name, birth_date, sexe, username, email, password) VALUES(:user_category_id, :school_id, :last_name, :first_name, :birth_date, :sexe, :username, :email, :password)");
+        $req->execute(compact('user_category_id', 'school_id', 'last_name', 'first_name', 'birth_date', 'sexe', 'username', 'email', 'password'));
+        echo "Mot de passe : " .$passwordn;
+    }
+
+    /**
      * @param string $username
      * @param string $password
      * @return bool
@@ -74,7 +125,7 @@ class User extends Model
     public function login(string $username, string $password) {
         $username = htmlspecialchars($username);
         $password = sha1($password);
-        $req = $this->pdo->prepare("SELECT * FROM {$this->table} WHERE username = :username AND password = :password");
+        $req = $this->pdo->prepare("SELECT * FROM {$this->table} WHERE (username = :username OR email = :username) AND password = :password");
         $req->execute(compact('username', 'password'));
         $user = $req->rowCount();
         if($user === 1) {
@@ -95,6 +146,59 @@ class User extends Model
             return false;
         }
 
+    }
+
+    /**
+     * @param string $first_name
+     * @param string $last_name
+     * @param string $email
+     * @return string
+     */
+    public function getDefaultUsername(string $first_name, string $last_name, string $email):string {
+        $username = explode("@", $email);
+        $username = $username[0];
+        $username = $username. "@_". $last_name.$first_name;
+        return $username;
+    }
+
+    /**
+     * @param string $username
+     * @return bool
+     */
+    public function verifyUsername(string $username):bool {
+        $req = $this->pdo->prepare("SELECT id FROM {$this->table} WHERE username = :username");
+        $req->execute(compact('username'));
+        if($req->rowCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @param string $email
+     * @return bool
+     */
+    public function verifyEmail(string $email):bool {
+        $req = $this->pdo->prepare("SELECT id FROM {$this->table} WHERE email = :email");
+        $req->execute(compact('email'));
+        if($req->rowCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @return false|string
+     * @throws \Exception
+     */
+    public static function generatePassword() {
+        $letterUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $letterLower = strtolower($letterUpper);
+        $integer = "0123456789";
+        $password = $letterUpper. $letterLower. $integer;
+        return substr(str_repeat(str_shuffle($password), random_int(self::PASSWORD_CHARACTER_REPEAT_MIN, self::PASSWORD_CHARACTER_REPEAT_MAX)), 0, self::PASSWORD_LENGTH);
     }
 
     /**
