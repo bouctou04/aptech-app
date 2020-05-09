a9d2d4883a195a3c871a322a8aac573100c0f050
bouctou04 <bmaiga04@gmail.com>
2020-05-09 21:39:43 +0000

    Liste des membres

--- views/followers.php
+++ views/followers.php
@@ -1,13 +1,25 @@
-<?php
-  
-?>
-<div class="col-12 col-lg-8">
-       <?php  
-            $user = new \Model\user();
-            echo"<h4 >Les Menbres de l'Administration</h4>"."<hr>";
-            $user->liste_user_admin($_SESSION['id'],$_SESSION['school_id']);
-            echo"<h4 >les etudiants</h4>"."<hr>";
-            $user->liste_users_student($_SESSION['id'],$_SESSION['school_id']);
-            
-        ?>
+<div class="col-12">
+    <h1 class="title mt-n3 font-weight-bold">Membres</h1>
+    <?php
+    $user = new \Model\User();
+    if(!empty($user->findAll())) {
+        foreach ($user->findAll("INNER JOIN user_category ON user_category.id = users.user_category_id") as $datas):
+            if($datas['id'] != $_SESSION['id']) { ?>
+                <ul class="collection">
+                    <li class="collection-item avatar">
+                        <img src="public/media/img/profile.jpg" alt="" class="circle">
+                        <a href="index.php?page=profile"><span class="title font-weight-bold">@<?= $datas['username'] ?></span></a>
+                        <p class="text-muted">
+                            <?= $datas['field'] ?>
+                        </p>
+                        <a href="index.php?page=message" class="secondary-content"><span class="fa fa-envelope"></span> Envoyer un Message</a>
+                    </li>
+                </ul>
+    <?php
+            }
+        endforeach;
+    } else {
+        echo "Pas de membre !";
+    }
+    ?>
 </div>
