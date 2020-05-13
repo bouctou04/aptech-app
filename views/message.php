2578ebd61bb6a10f6920f94509014376353f6e10
bouctou04 <bmaiga04@gmail.com>
2020-05-13 12:21:36 +0000

    Boîte de reception

--- views/message.php
+++ views/message.php
@@ -1,3 +1,32 @@
-<h1 class="title mt-n3 font-weight-bold">Message</h1>
-<?php
-redirectTo("index.php?page=maintenance");
+<div class="col-12">
+    <h1 class="title mt-n3 font-weight-bold">Boîte de réception</h1>
+    <div class="col-12">
+        <table class="table">
+            <tbody>
+            <tr>
+                <?php
+                $message = new \Model\Message();
+                if(!empty($message->getReception($_SESSION['id']))) {
+                    foreach ($message->getReception($_SESSION['id']) as $datas): ?>
+                            <?php
+                            if($_SESSION['id'] != $datas['sender_id']) { ?>
+                            <td class="row">
+                                <span class="font-weight-bold"><a href="index.php?page=read_message&&id=<?= $datas['sender_id'] ?>">@<?= $datas['username'] ?></a> </span>
+                                <span class="opacity-4 pl-2"><?= $datas['content'] ?></span>
+                                <span class="pl-3 opacity-1 small"><?= $datas['send_date'] ?></span>
+                            </td>
+                                <?php
+                            }
+                            ?>
+                    <?php
+                    endforeach;
+                } else { ?>
+                    <td class="row">Votre boîte de reception est vide ...</td>
+                    <?php
+                }
+                ?>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
